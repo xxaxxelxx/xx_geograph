3a58b25fbe95aab86c8289bc19b198e1c480cfbb
xxaxxelxx <x@axxel.net>
2016-01-22 15:25:20 +0100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 set -e
 #set -x
 
-wget http://www.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz > /dev/null
-if [ $? -eq 0 ]; then
-    mv -f GeoLiteCity.dat GeoLiteCity.dat.old 
-    gunzip -f GeoLiteCity.dat.gz > /dev/null
-    if [ $? -eq 0 ]; then
-	mv -f GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
-    fi
-fi
+wget http://www.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz > /dev/null 2>&1
+#if [ $? -eq 0 ]; then
+#    mv -f GeoLiteCity.dat GeoLiteCity.dat.old
+#    gunzip -f GeoLiteCity.dat.gz > /dev/null
+#    if [ $? -eq 0 ]; then
+#	mv -f GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
+#    fi
+#fi
 
-./geomat.sh $@
+#./geomat.sh $@
 
-#bash
+bash
 exit
